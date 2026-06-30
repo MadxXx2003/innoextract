65266f292c4b549b351f953af224c3128cfeb9a0
MadxXx2003 <mohamed.red2022@gmail.com>
2026-06-30 20:47:00 +0300

    Update slice.cpp

--- src/stream/slice.cpp
+++ src/stream/slice.cpp
@@ -26,7 +26,7 @@
 
 #include <boost/cstdint.hpp>
 #include <boost/algorithm/string/predicate.hpp>
-#include <boost/filesystem/operations.hpp>
+#include <boost/filesystem.hpp>
 // Debian Buster has Boost Filesystem 1.67.0.1 installed by default
 // directory.hpp was split from the main in 1.72
 // https://www.boost.org/users/history/version_1_72_0.html
